81c85dc1c5d7b629e9419dd7a2e02518f7b21d57
Lovely <lovelysarah146@gmail.com>
2025-02-20 23:19:35 +0530

    Add files via upload

---
+++ Java/Full Stack Java Development/AssignmentOperator.java
@@ -0,0 +1,13 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int x = 17;
+        x += 5;
+        x -= 5;
+        x *= 5;
+        x /= 4;
+        x %= 5;
+        System.out.println(x);
+
+
+    }
+}
