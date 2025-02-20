82bee154880bbca470b6ad935fdb96a890754fd0
Lovely <lovelysarah146@gmail.com>
2025-02-20 23:08:13 +0530

    Add files via upload

---
+++ Java/Full Stack Java Development/ArithmeticOperator.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperator {
+        public static void main(String[] args) {
+            int x = 12/2;
+            String text = "AB" + "CD";
+            System.out.println(x);
+            System.out.println(text);
+            int mod = 13%2;
+            System.out.println(x--);
+            System.out.println(x);
+            System.out.println(mod);
+        }
+
+}
