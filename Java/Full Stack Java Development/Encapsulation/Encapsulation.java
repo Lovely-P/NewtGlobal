ac662ab2b208e10399098b4fb927df1e2e84334a
Lovely <lovelysarah146@gmail.com>
2025-02-21 14:23:05 +0530

    Create Encapsulation.java

---
+++ Java/Full Stack Java Development/Encapsulation/Encapsulation.java
@@ -0,0 +1,27 @@
+ class Person {
+    public String name;
+    public int age;
+    public String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
+public class Encapsulation {
+        public static void main(String[] args) {
+            Person person= new Person("Lovely", 20, "Female");
+            System.out.println(person);
+
+        }
+}
