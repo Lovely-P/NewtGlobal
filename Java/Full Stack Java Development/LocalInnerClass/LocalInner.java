2915e5ba4f84fb2067b91699612039886f33b6b3
Lovely <lovelysarah146@gmail.com>
2025-02-22 09:08:45 +0530

    Create LocalInner.java

---
+++ Java/Full Stack Java Development/LocalInnerClass/LocalInner.java
@@ -0,0 +1,10 @@
+public class LocalInner {
+    public static void main(String[] args) {
+
+        if (new Door().isLocked("qwerty")){
+            System.out.println("Door is locked");
+        }else{
+            System.out.println("Door is unlocked");
+        }
+    }
+}
