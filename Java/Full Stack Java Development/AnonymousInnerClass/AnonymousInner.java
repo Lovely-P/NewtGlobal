73b16a0919892a0ec10b94ebcff6e703f0bbaf6b
Lovely <lovelysarah146@gmail.com>
2025-02-22 09:10:11 +0530

    Create AnonymousInner.java

---
+++ Java/Full Stack Java Development/AnonymousInnerClass/AnonymousInner.java
@@ -0,0 +1,14 @@
+public class AnonymousInner {
+    public static void main(String[] args) {
+        new Lock() {
+            @Override
+            public void isUnlocked(String key) {
+                if (key.equals("qwerty")){
+                    System.out.println("Shop is open");
+                }else
+                    System.out.println("Shop is closed");
+            }
+        }.isUnlocked("qwerty");
+
+    }
+}
