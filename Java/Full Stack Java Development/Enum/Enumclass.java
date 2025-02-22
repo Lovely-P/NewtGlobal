9d88dfa0d1583ce8769dd374ae03ca0acfcbaae3
Lovely <lovelysarah146@gmail.com>
2025-02-22 18:19:26 +0530

    Add files via upload

---
+++ Java/Full Stack Java Development/Enum/Enumclass.java
@@ -0,0 +1,7 @@
+public class Enumclass {
+    public static void main(String[] args) {
+        System.out.println(EnumExample.COLLECTIONS);
+        System.out.println(EnumExample.COREJAVA.getI());
+
+    }
+}
